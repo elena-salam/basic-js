dcd556526b01404d6ec771c7bbaf734b44f0e623
elena-salam <elena.salamakha.ua@gmail.com>
2021-03-28 17:24:35 +0300

    fix: fixed variable

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,11 +2,13 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(backyard) {
   let totalCat = 0;
-  const cat = "^^";
+  const cat = '^^';
 for(let i = 0; i<backyard.length; i +=1){
   for(let j = 0; j<backyard[i].length; j+=1){
-    if(backyard[i][j] == cat){
+    if(backyard[i][j] == (cat)){
       totalCat+=1;
+    } else {
+      totalCat +=0;
     }
   
   }
