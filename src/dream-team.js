92cd09dfbaa3156437306ac21faf529a2968aaca
elena-salam <elena.salamakha.ua@gmail.com>
2021-03-29 13:58:02 +0300

    feat: add solution for getting seasons

--- src/dream-team.js
+++ src/dream-team.js
@@ -9,16 +9,8 @@ return members.filter(el => typeof el=== "string")
   .map(el=>el.replace(/\s*/g, '').toUpperCase()[0]).sort()
   .join('');
 
-// return newArr.sort().join('')
-  // return members.filter(el => typeof el=== "string")
-  // .map(el=>el.replace(/\s*/g, '').toUpperCase()).sort()
-  // .join('');
+
   
-  // for(let i = 0; i<members.length; i+=1){
-  //   if(typeof members[i] === "string"){
-  //     return members[i].replace(/\s*/g, '');
-  //   }else{
-  //     return false
-  //   }
+  // .trim() - delete whitespaces as well
   
 };
